158ab59ea0cf57efe517ee5ecac332d71fe0134f
AdamRse <bakaarion3@gmail.com>
2026-06-11 16:52:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,6 @@ print(" ---------- Tips ----------")
 print("Exposant : ", 2**3)  # Exposant
 print("Ou exclusif : ", True ^ True)  # Ou exclusif (seulement 1 des 2 doit être true)
 print("in : ", "a" in ["a", "b", "c"])
+uneListe = []
+uneListe.append("coucou")
+print(uneListe)
