df55d410a49fff165b1d8e67427af730481a44be
AdamRse <adam.rousselle@proton.me>
2026-07-22 12:15:03 +0200

    y=ax+b

--- main_matplotlib.py
+++ main_matplotlib.py
@@ -18,8 +18,8 @@ axs[0,0].pie(
     labels=[f"gen {ng}" for ng in graph_gen.index],
     autopct=lambda pourcent: f"{round(pourcent, 2)}%\n({round(pourcent * graph_gen.sum() / 100)})"
 )
-
 axs[0,0].set_title('Distribution des générations')
+# --------------
 
 n, b, _ = axs[0,1].hist(df['Total'], bins=10, edgecolor="Black", color="skyblue")
 print(n, b)
@@ -30,8 +30,24 @@ axs[0,1].set_title('Distribution stats total')
 axs[0,1].set_xlabel('Stats totales')
 axs[0,1].set_ylabel('Nombre de Pokémon')
 axs[0,1].grid()
+# --------------
 
-axs[1,0].scatter(b[:-1], n)
+attaque = df['Attack']
+defense = df['Defense']
+axs[1,0].scatter(attaque, defense, alpha=0.2, color='blue', label='Pokémon')
+
+coeff = np.polyfit(attaque, defense, 1)  # données en abscisse, données en ordonnée, courbe recherchée (1=droite)
+fct_poly = np.poly1d(coeff) # renvoie une fct qui permet de calculer f(x) (f(x)=ax + b)
+print("polyfit : ",coeff)
+atk_min_max=[min(attaque),max(attaque)]
+axs[1,0].plot(atk_min_max, fct_poly(atk_min_max), color='red', label='Tendance')
+
+axs[1,0].set_title('Équilibrage')
+axs[1,0].set_xlabel('Attaque')
+axs[1,0].set_ylabel('Défense')
+axs[1,0].grid(True, alpha=0.6)
+axs[1,0].legend()
+# --------------
 
 #axs[1,0].pie(df.loc[df['Legendary'] == "True", 'Generation'].value_counts(), labels=["gen 1", "gen 2", "gen 3", "gen 4", "gen 5", "gen 6"])
 fig.suptitle("Stats pokémon")
